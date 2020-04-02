22db7e31dfbcf50685a1b64df91e3bbe6677fa68
chacemcneil <chacekmcneil@gmail.com>
2020-04-02 10:27:04 -0600

    Modify collapse button for markdown

---
+++ General Code/js/SortPlot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#SortPlot').scianimator({
+	    'images': ['images/SortPlot1.png', 'images/SortPlot2.png', 'images/SortPlot3.png', 'images/SortPlot4.png', 'images/SortPlot5.png', 'images/SortPlot6.png', 'images/SortPlot7.png', 'images/SortPlot8.png', 'images/SortPlot9.png', 'images/SortPlot10.png', 'images/SortPlot11.png', 'images/SortPlot12.png', 'images/SortPlot13.png', 'images/SortPlot14.png', 'images/SortPlot15.png', 'images/SortPlot16.png', 'images/SortPlot17.png', 'images/SortPlot18.png', 'images/SortPlot19.png', 'images/SortPlot20.png'],
+	    'width': 480,
+	    'delay': 200,
+	    'loopMode': 'loop'
+	});
+	$('#SortPlot').scianimator('play');
+    });
+})(jQuery);
